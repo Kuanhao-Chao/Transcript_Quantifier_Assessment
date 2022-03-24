df0934a4ad9e8681ee77ca950d6d7380330eb071
Yuchen Ge <yge15@salz1.idies.jhu.edu>
2022-03-23 20:13:41 -0400

    Add Step2 fastqc results

--- src/Step_2_fastqc.sh
+++ src/Step_2_fastqc.sh
@@ -1,1 +1,9 @@
-# FastQC on short+long reads
+# local variables
+HOME=/ccb/salz3/kh.chao/PR_StringTie_Quantification_Fix/Multi-map-Reads-Distributor
+DATA=/ccb/salz3/kh.chao/PR_StringTie_Quantification_Fix/data
+
+# make directory
+mkdir -p $HOME/results/fastqc
+
+# FastQC on short and long reads
+fastqc -o $HOME/results/fastqc -t 6 $DATA/short_reads/* $DATA/long_reads/*
